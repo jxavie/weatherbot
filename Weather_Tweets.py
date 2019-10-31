5e34beaa7504a9c292695b0dd62cf570630d5d5a
jxavie <54754878+jxavie@users.noreply.github.com>
2019-10-30 20:42:35 -0400

    adding files

---
+++ Weather_Tweets.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Dependencies
+import tweepy
+import time
+import json
+import random
+import requests as req
+import datetime
+
+import sys
+sys.path.append('../../../..')
+
+from config import consumer_key, consumer_secret, access_token, access_token_secret, weather_key
+
+
+# In[ ]:
+
+
+# Twitter API Keys
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
+
+
+# In[ ]:
+
+
+# Weather API Key
+url = "http://api.openweathermap.org/data/2.5/weather?"
+city = "Washington, D.C."
+units = "imperial"
+query_url = url + "appid=" + weather_key + "&q=" + city + "&units=" + units
+
+
+# In[ ]:
+
+
+# Create a function that gets the weather in London and Tweets it
+def WeatherTweet():
+
+    # Construct a Query URL for the OpenWeatherMap
+    url = "http://api.openweathermap.org/data/2.5/weather?"
+    city = "Washington, D.C."
+    units = "imperial"
+    query_url = url + "appid=" + weather_key + "&q=" + city + "&units=" + units
+
+    # Perform the API call to get the weather
+    r = req.get(query_url).json()
+    temp = r['main']['temp']
+
+    # Twitter credentials
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    # Tweet the weather
+    api.update_status(f"Current temperature in {city}: {temp}F")
+
+    # Print success message
+    print('Weather tweet posted')
+
+
+# In[ ]:
+
+
+# Set timer to run every 1 hour
+while(True):
+    WeatherTweet()
+    time.sleep(60*60)
+
+
+# In[ ]:
+
+
+
+
